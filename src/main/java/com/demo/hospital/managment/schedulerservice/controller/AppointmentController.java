835344a684342ca3ebcd50f544b972e711e925dc
Anagha Joshi <AnaghaJ2@citiustech.com>
2021-06-26 19:04:40 +0530

    Added API for view appointment by patient

--- src/main/java/com/demo/hospital/managment/schedulerservice/controller/AppointmentController.java
+++ src/main/java/com/demo/hospital/managment/schedulerservice/controller/AppointmentController.java
@@ -54,7 +54,14 @@ public class AppointmentController {
 	@GetMapping(path = "/getAppointmentToPatient")
 	public ResponseEntity<List<Appointment>> getAppointmentToPatient(@RequestParam Long patientId){
 		
-		return null;
+		try {
+			List<Appointment> listAppointment = appointmentServiceInteface.getAppointmentToPatient(patientId);
+			return new ResponseEntity<>(listAppointment,HttpStatus.OK);
+		}
+		catch (Exception e) {
+			e.getMessage();
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		
 	}
 	
--- src/main/java/com/demo/hospital/managment/schedulerservice/serviceinterface/AppointmentServiceInteface.java
+++ src/main/java/com/demo/hospital/managment/schedulerservice/serviceinterface/AppointmentServiceInteface.java
@@ -1,11 +1,10 @@
 package com.demo.hospital.managment.schedulerservice.serviceinterface;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.demo.hospital.managment.schedulerservice.entity.Appointment;
 
 public interface AppointmentServiceInteface {
 	public List<Appointment> getAppointmentToPhysician(Long physicianId);
-	public List<Appointment> getAppointmentToPatient(@RequestParam Long patientId);
+	public List<Appointment> getAppointmentToPatient(Long patientId);
 }
