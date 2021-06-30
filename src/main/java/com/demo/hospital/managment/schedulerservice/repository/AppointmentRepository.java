6cc8d73d199710d63040fbb68e76091151218b94
Anagha Joshi <AnaghaJ2@citiustech.com>
2021-06-30 20:26:50 +0530

    Resolved error of start date and end date

--- src/main/java/com/demo/hospital/managment/schedulerservice/repository/AppointmentRepository.java
+++ src/main/java/com/demo/hospital/managment/schedulerservice/repository/AppointmentRepository.java
@@ -5,16 +5,18 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 import com.demo.hospital.managment.schedulerservice.entity.Appointment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
 	
-	@Query("select a from Appointment a where a.physicianId = ?1 And a.startDate BETWEEN ?2 AND ?3 ")
+	@Query("select a from Appointment a where a.physicianId = ?1 And a.appointmentDate BETWEEN ?2 AND ?3 ")
 	List<Appointment> getAppointmentToPhysician(Long physicianId, LocalDate startDate, LocalDate endDate);
 	
-	@Query("select a from Appointment a where a.patientId = ?1 And a.startDate BETWEEN ?2 AND ?3")
+	@Query("select a from Appointment a where a.patientId = ?1 And a.appointmentDate BETWEEN ?2 AND ?3")
 	List<Appointment> getAppointmentToPatient(Long patientId, LocalDate startDate, LocalDate endDate);
 	
 	/*@Query("select a from Appointment a where a.physicianId = ?1 And a.patientId = ?2")
