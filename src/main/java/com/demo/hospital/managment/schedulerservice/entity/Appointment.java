c438eba47dff371aeadccbec94b49eed5971bf77
RathodSooraj <sanysuraj@gmail.com>
2021-07-17 10:04:27 +0530

    updated with the book appointment

--- src/main/java/com/demo/hospital/managment/schedulerservice/entity/Appointment.java
+++ src/main/java/com/demo/hospital/managment/schedulerservice/entity/Appointment.java
@@ -2,20 +2,18 @@ package com.demo.hospital.managment.schedulerservice.entity;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
 @Entity
@@ -25,7 +23,6 @@ public class Appointment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "appointment_id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long appointmentId;
 	@Column(name = "meeting_title")
 	@ApiModelProperty(notes = "Appointment Title")
@@ -44,5 +41,5 @@ public class Appointment {
 	@Column(name = "appointment_end_time")
 	private LocalTime appointmentEndTime;
 	@Column(name = "edit_id")
-	private Long editId;
+	private Long edit_id;
 }
